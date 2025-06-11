6fd2ad6ca1a8e280c02fca4a681ba1737e4972f7
Tawan-ok <saiordday1@gmail.com>
2025-06-11 11:49:06 +0700

    add update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import Product from './model/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -23,6 +24,54 @@ app.post('/api/products', async (req, res) => {
     res.status(201).json(newProduct);
   } catch (error) {
     res.status(500).json({ message: 'Error creating product' });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting product' });
+  }
+});
+
+app.get("/api/products", async ( res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching products' });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching product' });
+  }
+});
+
+  app.put("/api/products/:id", async (req, res) => {
+  const {id} = req.params;
+  const product = req.body;
+
+if(!mongoose.Types.ObjectId.isValid(id)) {
+  return res.status(400).json({ message: 'Invalid product ID' });
+}
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating product' });
   }
 });
 
