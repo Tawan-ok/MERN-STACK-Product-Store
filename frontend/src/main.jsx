63d338d4f677a0afab9c8ffacb4beb485653472a
Tawan-ok <saiordday1@gmail.com>
2025-06-12 22:42:00 +0700

    add front-end

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Provider } from "@/components/ui/provider"
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
